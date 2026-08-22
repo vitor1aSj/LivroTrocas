c89069f6523234c7237745a4146f61637d5540a9
vitor1aSj <vitoria.vih.silvah@gmail.com>
2026-08-21 21:14:38 -0300

    sal

--- praticando.js/home.js
+++ praticando.js/home.js
@@ -2,28 +2,64 @@ const nomeEntrada = document.getElementById("nome-entrada");
 const nomeSaida = document.getElementById("nome-saida");
 const btnTroca = document.getElementById("btn-troca");
 
-btnTroca.addEventListener( 'click', () => {
+btnTroca.addEventListener('click', () => {
     nomeSaida.textContent = nomeEntrada.value;
 });
 
-btnTroca.addEventListener( ' mouseenter ', () => {
+btnTroca.addEventListener(' mouseenter ', () => {
     nomeSaida.style.color = "#5c1b1b";
 });
 
-btnTroca.addEventListener( ' mouseout', () => {
+btnTroca.addEventListener(' mouseout', () => {
     nomeSaida.style.color = " #780303"
 
 })
 
 //trocando a cor da caixa 
 
-const btnVermelho = document.getElementById("btn-vermelho");
-const btnPreto = document.getElementById("btn-preto");
-const btnBranco = document.getElementById("btn-branco");
+const btnVermelho = document.getElementById("btn_vermelho");
+const btnPreto = document.getElementById("btn_preto");
+const btnBranco = document.getElementById("btn_branco");
 const caixa = document.getElementById("caixa");
 
-btnVermelho.addEventListener('click', function(){
-    caixa.style.backgroundColor
+btnVermelho.addEventListener('click', () => {
+    caixa.style.backgroundColor = "red";
+});
 
+btnPreto.addEventListener('click', () => {
+    caixa.style.backgroundColor = "black";
+});
 
-})
+btnBranco.addEventListener('click', () => {
+    caixa.style.backgroundColor = "white";
+});
+
+//simulando add e sub de valor 
+const valorReal = document.getElementById("valor_real");
+let contador = 0;
+
+document.getElementById("btn_menos").addEventListener('click', () => {
+    if (contador > 0) {
+        contador--;
+        valorReal.textContent = contador;
+    }
+});
+
+document.getElementById("btn_mais").addEventListener('click', () => {
+    contador++;
+    valorReal.textContent = contador;
+});
+
+//add livros
+
+const nomeLivro = document.getElementById("nome_livro");
+const listaLivros = document.getElementById("lista_livros");
+
+document.getElementById("adiciona_livro").addEventListener('click', () => {
+
+    const novoLivro = document.createElement("li");
+    novoLivro.textContent = nomeLivro.value;
+    listaLivros.appendChild(novoLivro);
+
+    nomeLivro.value = "";
+});
